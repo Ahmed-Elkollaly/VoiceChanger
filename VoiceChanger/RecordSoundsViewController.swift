988aea221c446a874f72e5feee38f67d3f3529d5
Ahmed El-Kollaly <ahmed_elkollaly@Mohameds-MacBook-Air.local>
2017-09-02 17:01:03 +0200

    Add record and stop buttons and a label and configure UI for them

--- VoiceChanger/RecordSoundsViewController.swift
+++ VoiceChanger/RecordSoundsViewController.swift
@@ -9,17 +9,44 @@
 import UIKit
 
 class RecordSoundsViewController: UIViewController {
-
+    
+    //Properties
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var recordLabel: UILabel!
+    @IBOutlet weak var stopButton: UIButton!
+    enum PlayingState {case playing,notPlaying}
+    var playingState :PlayingState = .notPlaying
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        configureUI()
     }
-
+    //Mark -Configure UI
+    func configureUI(){
+        switch playingState {
+        case .playing:
+            recordButton.isEnabled = false
+            stopButton.isEnabled = true
+            recordLabel.text = "Recording ..."
+        case .notPlaying:
+            recordButton.isEnabled = true
+            stopButton.isEnabled = false
+            recordLabel.text = "Tap to record"
+        }
+    }
+    //Mark -Start Recording
+    @IBAction func startRecording(_ sender: Any) {
+        playingState = .playing
+        configureUI()
+    }
+    //Mark - Stop Recording
+    @IBAction func stopRecording(_ sender: Any) {
+        playingState = .notPlaying
+        configureUI()
+    }
 
 }
 
